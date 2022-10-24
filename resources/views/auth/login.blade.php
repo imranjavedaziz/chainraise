ba609c1a333269fa77fdc07c2f10f778acf7419b
Tayyab <tayyab.zino19@gmail.com>
2022-10-24 20:33:41 +0500

    Login Title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <head>
     <base href="../../../../">
     <meta charset="utf-8" />
-    <title>Login Page 5 | Keenthemes</title>
+    <title> Login Chainraise </title>
     <meta name="description" content="Login page example" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="canonical" href="https://keenthemes.com/metronic" />
