1ce0a4d4d7d6b15ee2bbeaf3c171cf014fdea7bd
Tayyab <tayyab.zino19@gmail.com>
2022-11-07 10:55:33 +0500

    Api Redirect4.6

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-//Route::get('dummer', [UserController::class, 'dummy'])->name('dummer');
+Route::get('dummer', [UserController::class, 'dummy'])->name('dummer');
+
 Route::get('custom_login/{email}/{password}', [UserController::class, 'custom_login']);
 
 Route::get('/', function () {
