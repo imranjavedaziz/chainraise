4379dad40034be852ac4b68c062e0a6e3d4fb1ee
Tayyab <tayyab.zino19@gmail.com>
2022-12-19 02:57:13 +0500

    New Repo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 <head>
 
-    <title>Chainraise | Login 3.1</title>
+    <title>Chainraise | Login 4.1</title>
     <meta charset="utf-8" />
     <meta name="description"
         content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Flask & Laravel versions. Grab your copy now and get life-time updates for free." />
