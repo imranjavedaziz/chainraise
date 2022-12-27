8584af938bc6876891f1b0fe3e9998744fa58aba
Tayyab <tayyab.zino19@gmail.com>
2022-12-28 00:34:22 +0500

    Delete User and seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -48,92 +48,92 @@ class UserSeeder extends Seeder
 
 
 
-        $user = new User;
-        $user->name = "investor";
-        $user->email = "investor@gmail.com";
-        $user->password = bcrypt("Google@123");
-        $user->phone = "000000000";
-        $user->agree_consent_electronic = true;
-        $user->status = 'active';
-        $user->parent_id = 2;
-        $user->is_primary = true;
-        $user->save();
-        $user->assignRole('investor');
+        // $user = new User;
+        // $user->name = "investor";
+        // $user->email = "investor@gmail.com";
+        // $user->password = bcrypt("Google@123");
+        // $user->phone = "000000000";
+        // $user->agree_consent_electronic = true;
+        // $user->status = 'active';
+        // $user->parent_id = 2;
+        // $user->is_primary = true;
+        // $user->save();
+        // $user->assignRole('investor');
 
-        $user_detail = new UserDetail;
-        $user_detail->user_id = $user->id;
-        $user_detail->middle_name = "Middle Name";
-        $user_detail->last_name = "Last Nmae";
-        $user_detail->title = "Title";
-        $user_detail->dob = '1992-02-15';
-        $user_detail->address = "Address";
-        $user_detail->suit = "Suit";
-        $user_detail->city = "City";
-        $user_detail->state = "State";
-        $user_detail->zip = "00000";
-        $user_detail->legal_formation = "Legal Formation";
-        $user_detail->date_incorporation ="date_incorporation";
-        $user_detail->save();
-
-
-
-
-        $user = new User;
-        $user->name = "issuer";
-        $user->email = "issuer@gmail.com";
-        $user->password = bcrypt("Google@123");
-        $user->phone = "000000000";
-        $user->agree_consent_electronic = true;
-        $user->status = 'active';
-        $user->parent_id = 3;
-        $user->is_primary = true;
-        $user->save();
-        $user->assignRole('issuer');
-
-
-        $user_detail = new UserDetail;
-        $user_detail->user_id = $user->id;
-        $user_detail->middle_name = "Middle Name";
-        $user_detail->last_name = "Last Nmae";
-        $user_detail->title = "Title";
-        $user_detail->dob = '1992-02-15';
-        $user_detail->address = "Address";
-        $user_detail->suit = "Suit";
-        $user_detail->city = "City";
-        $user_detail->state = "State";
-        $user_detail->zip = "00000";
-        $user_detail->legal_formation = "Legal Formation";
-        $user_detail->date_incorporation ="date_incorporation";
-        $user_detail->save();
+        // $user_detail = new UserDetail;
+        // $user_detail->user_id = $user->id;
+        // $user_detail->middle_name = "Middle Name";
+        // $user_detail->last_name = "Last Nmae";
+        // $user_detail->title = "Title";
+        // $user_detail->dob = '1992-02-15';
+        // $user_detail->address = "Address";
+        // $user_detail->suit = "Suit";
+        // $user_detail->city = "City";
+        // $user_detail->state = "State";
+        // $user_detail->zip = "00000";
+        // $user_detail->legal_formation = "Legal Formation";
+        // $user_detail->date_incorporation ="date_incorporation";
+        // $user_detail->save();
 
 
 
-        $user = new User;
-        $user->name = "issuer2";
-        $user->email = "issuer2@gmail.com";
-        $user->password = bcrypt("Google@123");
-        $user->phone = "000000044";
-        $user->agree_consent_electronic = true;
-        $user->status = 'active';
-        $user->parent_id = 3;
-        $user->is_primary = true;
-        $user->save();
-        $user->assignRole('issuer');
+
+        // $user = new User;
+        // $user->name = "issuer";
+        // $user->email = "issuer@gmail.com";
+        // $user->password = bcrypt("Google@123");
+        // $user->phone = "000000000";
+        // $user->agree_consent_electronic = true;
+        // $user->status = 'active';
+        // $user->parent_id = 3;
+        // $user->is_primary = true;
+        // $user->save();
+        // $user->assignRole('issuer');
 
 
-        $user_detail = new UserDetail;
-        $user_detail->user_id = $user->id;
-        $user_detail->middle_name = "Middle Name";
-        $user_detail->last_name = "Last Nmae";
-        $user_detail->title = "Title";
-        $user_detail->dob = '1992-02-15';
-        $user_detail->address = "Address";
-        $user_detail->suit = "Suit";
-        $user_detail->city = "City";
-        $user_detail->state = "State";
-        $user_detail->zip = "00000";
-        $user_detail->legal_formation = "Legal Formation";
-        $user_detail->date_incorporation ="date_incorporation";
-        $user_detail->save();
+        // $user_detail = new UserDetail;
+        // $user_detail->user_id = $user->id;
+        // $user_detail->middle_name = "Middle Name";
+        // $user_detail->last_name = "Last Nmae";
+        // $user_detail->title = "Title";
+        // $user_detail->dob = '1992-02-15';
+        // $user_detail->address = "Address";
+        // $user_detail->suit = "Suit";
+        // $user_detail->city = "City";
+        // $user_detail->state = "State";
+        // $user_detail->zip = "00000";
+        // $user_detail->legal_formation = "Legal Formation";
+        // $user_detail->date_incorporation ="date_incorporation";
+        // $user_detail->save();
+
+
+
+        // $user = new User;
+        // $user->name = "issuer2";
+        // $user->email = "issuer2@gmail.com";
+        // $user->password = bcrypt("Google@123");
+        // $user->phone = "000000044";
+        // $user->agree_consent_electronic = true;
+        // $user->status = 'active';
+        // $user->parent_id = 3;
+        // $user->is_primary = true;
+        // $user->save();
+        // $user->assignRole('issuer');
+
+
+        // $user_detail = new UserDetail;
+        // $user_detail->user_id = $user->id;
+        // $user_detail->middle_name = "Middle Name";
+        // $user_detail->last_name = "Last Nmae";
+        // $user_detail->title = "Title";
+        // $user_detail->dob = '1992-02-15';
+        // $user_detail->address = "Address";
+        // $user_detail->suit = "Suit";
+        // $user_detail->city = "City";
+        // $user_detail->state = "State";
+        // $user_detail->zip = "00000";
+        // $user_detail->legal_formation = "Legal Formation";
+        // $user_detail->date_incorporation ="date_incorporation";
+        // $user_detail->save();
     }
 }
