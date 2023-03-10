2f64192301b090f9fb9ec205629e80d1978874db
unknown <tayyab.shahzad@ikonicsolition.com>
2023-03-11 01:02:28 +0500

    remove must verify

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 use App\Notifications\ResetPasswordNotification;
 use Illuminate\Contracts\Auth\CanResetPassword;
 
-class User extends Authenticatable  implements MustVerifyEmail, HasMedia 
+class User extends Authenticatable  implements HasMedia 
 {
 
     use HasApiTokens, HasFactory, Notifiable, HasRoles,InteractsWithMedia;
